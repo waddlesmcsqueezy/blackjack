710e6859d674eea9ef097c493311e6caa20b928c
waddlesmcsqueezy <waddles1337@gmail.com>
2019-05-03 18:54:08 -0500

    added player class and new functions for blackjack

--- blackjack.py
+++ blackjack.py
@@ -1,12 +1,74 @@
 import json
 import requests
+import enum
 
 link = "http://deckofcardsapi.com/api/deck"
+game_type = ""
 
-deck_count = 6
+# Enum GameType
+# 	Used for determining what game the user is playing
 
-decks = requests.get ( link + "/new/shuffle/?deck_count=" + str ( deck_count ) )
+class GameType(Enum):
+	BLACKJACK = "blackjack"
+	
+# Class Player
+# 	
 
-decks = decks.json ( )
+class Player:
+	def __init__( self ):
+		self.hand = None
+		self.bet = 0
 
-print ( decks )
+	def placeBet( ):
+		
+	def dealCard( ):
+		
+
+# Function createDecks
+# 	Gets new deck(s) of 52 cards and stores them in variable decks
+# Receives: amount of decks (int)
+# Returns: n/a
+
+def createDecks( deck_count ):
+	decks = requests.get( link + "/new/shuffle/?deck_count=" + str( deck_count ) )
+
+	decks = decks.json( )
+
+# Function setGame:
+# 	Edits the global string variable game_type to the appropriate type. 
+# 	Prints error message if user_input is an invalid choice.
+# Receives: user input (str)
+# Returns: n/a
+
+def setGameType( str( user_input ).lower( ) ):
+	for game in GameType:
+		if user_input == game:
+			game = user_input
+		else:
+			raise Exception( '''Game type should correspond to available game types.\n
+					    Allowed game types are:\n''' + [game for game in GameType])
+# Function prompt
+# Receives: optional string to be displayed before input prompt (str)
+# Returns: user input (str)
+
+def prompt( optional_string = "" ):
+	if optional_string != "":
+		print( optional_string )
+
+	print( "Enter Command: " )
+
+	user_input = input( )
+	
+	return user_input
+
+def inputLoop( ):
+	while user_input != "exit":
+		prompt( )
+
+def main( ):
+	decks = createDecks( 6 )
+	
+	print( '''Welcome to Blackjack\n
+		  Type /help for commands list''' )
+	
+	inputLoop( )
